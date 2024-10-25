f49d2e9dc4e586fdacc4a00f02fee0212f00001f
Insaf BELHAFI <ibelhafi.ext@lebonmarche.fr>
2024-10-25 16:12:23 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Bad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WebApplication1
+{
+    public class Bad
+    {
+        void DoPrint(object o)
+        {
+            Console.WriteLine(o.ToString());
+        }
+        public void M()
+        {
+            DoPrint("Hello");
+            DoPrint(null);
+            Logger.Log("Hello, World!");
+        }
+
+        static class Logger
+        {
+            [Obsolete("Use Log(LogLevel level, string s) instead")]
+            public static void Log(string s)
+            {
+                // ...
+            }
+
+            public static void Log(LogLevel level, string s)
+            {
+                // ...
+            }
+        }
+
+        enum LogLevel
+        {
+            Info,
+            Warning,
+            Error
+        }
+    }
+}
